54af8c7b03839c36b8cd95184c8bc45e7c3aea67
unknown <mslyusarev@MSLYUSAREV-TEST.universe.dart.spb>
2015-07-21 20:23:22 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Getting and Cleaning Data course project ##
+
+This project aims to be a course project for Getting and Cleaning Data class from Coursera.
---
+++ run_analysis.R
@@ -0,0 +1,8 @@
+data_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+train_path <- 'UCI HAR Dataset/train'
+test_path <- 'UCI HAR Dataset/test'
+
+temp_zip <- tempfile()
+download.file(data_url, temp_zip)
+#data <- read.table(unz(temp_zip, ))
+unlink(temp)
