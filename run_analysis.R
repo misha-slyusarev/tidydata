6867ffbb1fcaae3344ef204556eb969a8b07c71b
Misha Slyusarev <mik3weider@gmail.com>
2015-07-23 21:12:37 +0300

    Adds paths.

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,16 @@
 data_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-train_path <- 'UCI HAR Dataset/train'
-test_path <- 'UCI HAR Dataset/test'
+
+root_path <- 'UCI HAR Dataset'
+train_path <- paste(root_path, 'train', sep = '/')
+test_path <- paste(root_path, 'test', sep = '/')
+
+features_path <- paste(root_path, 'features.txt', sep = '/')
 
 temp_zip <- tempfile()
 download.file(data_url, temp_zip)
-#data <- read.table(unz(temp_zip, ))
+
+features <- read.table(unz(temp_zip, features_path))
+
+View(features)
+
 unlink(temp_zip)
