9ff80ec9d5cea172db11ffb24ba2ff10b3f9f520
Misha Slyusarev <mik3weider@gmail.com>
2015-07-25 17:35:56 +0300

    Read feature names and set columns for the test and train data

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,32 @@
 
 data_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 
-if( ! file.exists(temp_zip) ){
+root_path <- 'UCI HAR Dataset'
+train_data_path <- paste(root_path, 'train', sep = '/')
+test_data_path <- paste(root_path, 'test', sep = '/')
+
+if( ! file.exists(root_path) ){
   temp_zip <- tempfile()
   download.file(data_url, temp_zip)
+  unzip(temp_zip)
+  unlink(temp_zip)
 }
 
-root_path <- 'UCI HAR Dataset'
-train_data_path <- paste(root_path, 'train/X_train.txt', sep = '/')
-test_data_path <- paste(root_path, 'test/X_test.txt', sep = '/')
+feature_names <- read.table(paste(root_path, 'features.txt', sep = '/'),
+                       row.names = 1, col.names = c("ID", "Name"))
 
-data_train <- read.table(unz(temp_zip, train_data_path))
-data_test <- read.table(unz(temp_zip, test_data_path))
+data_train <- read.table(paste(train_data_path, 'X_train.txt', sep = '/'))
+data_train_labels <- read.table(paste(train_data_path, 'y_train.txt', sep = '/'))
 
-print( dim(data_train) )
-print( dim(data_test) )
+data_test <- read.table(paste(test_data_path, 'X_test.txt', sep = '/'))
+data_test_labels <- read.table(paste(test_data_path, 'y_test.txt', sep = '/'))
 
-unlink(temp_zip)
+colnames(data_train) <- feature_names$Name
+colnames(data_test) <- feature_names$Name
+
+#print( dim(data_train) )
+#print( dim(data_test) )
+
+#activity_labels <- read.table(paste(root_path, 'activity_labels.txt', sep='/'),
+#                              col.names = c('Label', 'Name'))
+#View(activity_labels)
