3fb2c7014baebfe1d61a3fb8c23e208bf1368559
Misha Slyusarev <mik3weider@gmail.com>
2015-07-25 16:17:47 +0300

    Adds check for file existence

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,19 @@
+
 data_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 
+if( ! file.exists(temp_zip) ){
+  temp_zip <- tempfile()
+  download.file(data_url, temp_zip)
+}
+
 root_path <- 'UCI HAR Dataset'
-train_path <- paste(root_path, 'train', sep = '/')
-test_path <- paste(root_path, 'test', sep = '/')
+train_data_path <- paste(root_path, 'train/X_train.txt', sep = '/')
+test_data_path <- paste(root_path, 'test/X_test.txt', sep = '/')
 
-features_path <- paste(root_path, 'features.txt', sep = '/')
+data_train <- read.table(unz(temp_zip, train_data_path))
+data_test <- read.table(unz(temp_zip, test_data_path))
 
-temp_zip <- tempfile()
-download.file(data_url, temp_zip)
-
-features <- read.table(unz(temp_zip, features_path))
-
-View(features)
+print( dim(data_train) )
+print( dim(data_test) )
 
 unlink(temp_zip)
